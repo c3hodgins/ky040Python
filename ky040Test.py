66654bf415667cb07b1196e4aa88b4ffa5226b07
c3hodgins <c3hodgins@gmail.com>
2024-03-17 17:10:20 -0400

    ky040Test.py Rotary Encoder Example tracked

---
+++ ky040Test.py
@@ -0,0 +1,41 @@
+from ky040 import ky040
+from gpiozero import DigitalOutputDevice
+from time import sleep
+
+enc = ky040(15,14,18)
+led = DigitalOutputDevice(4)
+
+count = 0
+last = 0
+direction = 0
+pwmCtr = 0
+incVal = 20
+
+with open('output.txt','w') as f:
+    f.write("Encoder Count: ")
+    sleep(.01)
+    while(1):
+        pwmCtr+=1
+        last = count
+        direction = enc.dir()
+        if direction== 1:
+            if count<999:
+                count+=incVal
+        elif direction == -1:
+            if count>1:
+                count -=incVal 
+            else:
+                count = 0 
+        if direction !=0 and last !=count:
+                f.seek(len("Encoder Count: "))
+                f.write(str(count))
+                f.truncate()
+                f.flush()
+            # print("Encoder: ",count)
+        if pwmCtr < count:
+            led.value = 1
+        else:
+            led.value = 0
+        if pwmCtr>=1000:
+            pwmCtr = 0
+            sleep(.000000000003)
